fc940d437a847e9dbc2bd15ac38d9a3a46d6f3eb
Szymon Sysło <szymonsyslo@gmail.com>
2019-09-14 16:08:38 +0200

    small fix

--- Grid.js
+++ Grid.js
@@ -1,45 +1,45 @@
 class Grid {
-   constructor(root) {
-      this._root = document.getElementById(root);
-      this._squares = [];
-   }
+  constructor(root) {
+    this._root = document.getElementById(root);
+    this._squares = [];
+  }
 
-   get root() {
-      return this._root;
-   }
+  get root() {
+    return this._root;
+  }
 
-   set root(root) {
-      return this._root = root;
-   }
+  set root(root) {
+    return (this._root = root);
+  }
 
-   get squares() {
-      return this._squares;
-   }
+  get squares() {
+    return this._squares;
+  }
 
-   renderSquare(wrapper, square, index) {
-      const elem = document.createElement('div');
+  renderSquare(wrapper, square, index) {
+    const elem = document.createElement("div");
 
-      elem.id = `sq-${index + 1}`;
-      elem.classList.add('game-square');
-      elem.dataset.value = square.value;
-      elem.dataset.order = square.order;
-      elem.dataset.isActive = square.isActive.toString();
-      elem.dataset.isMatched = square.isMatched.toString();
-      elem.style.backgroundColor = square.color;
-      elem.textContent = square.value;
+    elem.id = `sq-${index + 1}`;
+    elem.classList.add("game-square");
+    elem.dataset.value = square.value;
+    elem.dataset.order = square.order;
+    elem.dataset.isActive = square.isActive.toString();
+    elem.dataset.isMatched = square.isMatched.toString();
+    elem.style.backgroundColor = square.color;
+    // elem.textContent = square.value;
 
-      wrapper.appendChild(elem)
-   }
+    wrapper.appendChild(elem);
+  }
 
-   render(squares) {
-      this.root.innerHTML = '';
+  render(squares) {
+    this.root.innerHTML = "";
 
-      const wrapper = document.createDocumentFragment();
+    const wrapper = document.createDocumentFragment();
 
-      squares.forEach((square, index) => {
-         this.renderSquare(wrapper, square, index);
-      });
+    squares.forEach((square, index) => {
+      this.renderSquare(wrapper, square, index);
+    });
 
-      this.root.appendChild(wrapper);
-   }
+    this.root.appendChild(wrapper);
+  }
 }
